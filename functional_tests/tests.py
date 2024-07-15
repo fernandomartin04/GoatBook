963e2c7beac38a0fc3fdf3b45545766f18f7be97
fernandomartin04 <fernandomartin04@iesamachado.org>
2024-07-15 14:26:07 +0200

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,21 +1,27 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.common.keys import Keys 
 import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase) :
+class NewVisitorTest(LiveServerTestCase) :
     def setUp(self) : #Antes de realizar el test(self)
         self.browser = webdriver.Firefox()
 
     def tearDown(self) : #Tras realizar el test(self)
         self.browser.quit() 
-    
+
+    def check_for_row_in_list_table(self, row_text) :
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_todo_list(self) :
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
 
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
         # She notices the page title and header mention to-do lists
         
         self.assertIn("To-Do", self.browser.title)
@@ -35,8 +41,8 @@ class NewVisitorTest(unittest.TestCase) :
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
-        table = self.browser.find_element(By.ID, "id_list_table")
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly" 
         # (Edith is very methodical)
@@ -48,22 +54,8 @@ class NewVisitorTest(unittest.TestCase) :
 
         # The page updates again, and now shows both items on her list
 
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn(
-            "1: Buy peacock feathers", 
-            [row.text for row in rows],
-        )
-        self.assertIn(
-            "2: Use peacock feathers to make a fly", 
-            [row.text for row in rows],
-        )
-        
-
-        
-        
-
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+      
         # Satisfied, she goes back to sleep
 
-if __name__ == "__main__":
-    unittest.main()
