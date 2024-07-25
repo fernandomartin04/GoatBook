75a6e6749890174f3e89555d28cf8a84991fdd7d
fernandomartin04 <fernandomartin04@iesamachado.org>
2024-07-25 11:01:36 +0200

    Split out unit tests into two files

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -47,34 +47,6 @@ class ListViewTests(TestCase) :
         response = self.client.get(f"/lists/{correct_list.id}/")
         self.assertEqual(response.context["list"], correct_list)
 
-class ListAndItemModelTest(TestCase) :
-    def test_saving_and_retrieving_items(self) :
-        mylist = List()
-        mylist.save()
-
-        first_item = Item()
-        first_item.text = "The first (ever) list item"
-        first_item.list = mylist
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "Item the second"
-        second_item.list = mylist
-        second_item.save()
-
-        saved_list = List.objects.get()
-        self.assertEqual(saved_list, mylist)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "The first (ever) list item")
-        self.assertEqual(second_saved_item.list, mylist)
-        self.assertEqual(second_saved_item.text, "Item the second")
-        self.assertEqual(second_saved_item.list, mylist)
-
 class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
